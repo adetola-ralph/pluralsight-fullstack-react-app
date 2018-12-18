35a3cfad250cce427104f0ae59fbef1a79ac5749
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-12-18 22:25:30 +0100

    Configure webpack for express

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
+import fs from 'fs-extra';
+import morgan from 'morgan';
+import webpack from 'webpack';
 import express from 'express';
 import yields from 'express-yields';
 import fs from 'fs-extra';
 
-const port = process.eventNames.PORT || '1246';
 const app = express();
+const port = process.eventNames.PORT || '1246';
+
+if (process.env.NODE_ENV === 'development') {
+  const config = require('../webpack.config.dev.babel').default;
+  const compiler = webpack(config);
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+  }));
+
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
 app.get(['/'], function* (req, res) {
   let index = yield fs.readFile('./public/index.html', 'utf-8');
---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,97 @@
+import path from 'path';
+import webpack from 'webpack';
+// import merge from 'webpack-merge';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+// import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+  title: '',
+});
+
+// const miniCssPlugin = new MiniCssExtractPlugin({
+//   filename: "[name].css",
+//   chunkFilename: "[id].css"
+// });
+
+export default {
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    'babel-regenerator-runtime',
+    path.join(__dirname, 'src/index.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  target: 'web',
+  resolve: {
+  //   alias: {
+  //     '~': path.resolve(__dirname, 'node_modules'),
+  //   },
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [
+    htmlPlugin,
+    // miniCssPlugin,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'development',
+        WEBPACK: true,
+      }
+    }),
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      // {
+      //   test: /\.css$/,
+      //   loaders: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+      // },
+      // {
+      //   test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      //   loader: 'file-loader',
+      // },
+      // {
+      //   test: /\.(woff|woff2)$/,
+      //   loader: 'url-loader?prefix=font/&limit=5000',
+      // },
+      // {
+      //   test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      //   loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      // },
+      // {
+      //   test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      //   loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      // },
+      // {
+      //   enforce: 'pre',
+      //   test: /\.jsx?$/,
+      //   include: path.join(__dirname, 'src'),
+      //   exclude: /node_modules/,
+      //   loaders: ['eslint-loader'],
+      // },
+    ],
+  },
+  mode: 'development',
+  // devtool: 'inline-source-map',
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, 'src'),
+  //   hot: true,
+  //   open: true,
+  //   historyApiFallback: true,
+  //   disableHostCheck: true,
+  // },
+};
