2b854816ed285112297f354356d70064e1133c0a
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-12-19 12:02:58 +0100

    Add route for adding and getting groceryitems

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,28 @@
+import boom from 'boom';
+import GroceryItem from '../model/GroceryItems';
+
 export default (router) => {
   router.get('/', function* (req, res) {
     res.send('hey there, howdy');
   });
 
-  router.get('/items', function* (req, res) {
-    res.json([
-      {
-        name: 'Ice Cream',
-      },
-      {
-        name: 'Hot dog',
-      },
-    ]);
-  });
+  router.route('/items')
+    .get(function* (req, res) {
+      try {
+        const groceryItems = yield GroceryItem.find().exec();
+        return res.json(groceryItems.map(item => item.toJSON()));
+      } catch (err) {
+        throw boom.badImplementation('Server error, please try again later', err);
+      }
+    })
+    .post(function* (req, res) {
+      const item = req.body;
+
+      try {
+        const groceryItem = yield new GroceryItem(item).save();
+        res.status(201).json(groceryItem);
+      } catch (err) {
+        throw boom.badImplementation('Server error, please try again later', err);
+      }
+    });
 };
